efc2e85238cfc103a464f536ebdc80f8ab99f707
Amos Gheva <ghevaamos@gmail.com>
2025-08-31 08:27:23 -0400

    Getting info about a contract experiment

---
+++ autotrader/experiments/contract_details.cpp
@@ -0,0 +1,79 @@
+#include "ib/ib_client.h"
+#include "Contract.h"
+#include <future>
+#define DEBUG
+#include "debug.h"
+
+using autotrader::get_log_stream;
+
+class Client : public autotrader::IBClient
+{
+public:
+  Client(std::string host, int port);
+  void contract_details(Contract& contract);
+  virtual void contractDetails( int reqId, const ContractDetails& contractDetails) override;
+
+  long contract_id(Contract contract);
+private:
+  void req_contract(Contract contract);
+  bool asked_;
+  Contract asked_for_;
+};
+
+Client::Client(std::string host, int port) :
+  autotrader::IBClient(host, port), asked_{false}
+{
+}
+
+void Client::contract_details(Contract& contract)
+{
+  client()->reqContractDetails(777, contract);
+}
+
+void Client::contractDetails( int reqId, const ContractDetails& contractDetails)
+{
+  asked_ = true;
+  asked_for_ = contractDetails.contract;
+  //DBG_MSG("Contract Details:") << "id: " << contractDetails.contract.conId << std::endl;
+}
+
+void Client::req_contract(Contract contract)
+{
+  if (!asked_)
+  {
+    std::future<void> query = std::async([this, &contract] -> void {
+        contract_details(contract);
+        step();
+        });
+    query.get();
+  }
+}
+
+long Client::contract_id(Contract contract)
+{
+  req_contract(contract);
+  return asked_for_.conId;
+}
+
+int main(int argc, char** argv)
+{
+  if (argc != 3)
+  {
+    throw std::runtime_error("Expecting host and port arguments");
+  }
+  std::string host{argv[1]}; // host.docker.internal
+  int port{std::atoi(argv[2])}; // 7497
+  Client client(host, port);
+
+  Contract contract;
+  contract.symbol = "INTC";
+  contract.secType = "STK";
+  contract.currency = "USD";
+  contract.exchange = "SMART";
+
+  client.start();
+  DBG_MSG("Contract ID is:") << client.contract_id(contract) << std::endl;
+
+  return 0;
+}
+

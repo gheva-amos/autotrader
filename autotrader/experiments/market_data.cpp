3f02d42c1622ac507d8ac28fce88fbbf34e13493
Amos Gheva <ghevaamos@gmail.com>
2025-09-04 09:23:54 -0400

    A small experiment to tests the market data

---
+++ autotrader/experiments/market_data.cpp
@@ -0,0 +1,31 @@
+#include "ib/ib_client.h"
+#include <thread>
+#include <chrono>
+#define DEBUG
+#include "debug.h"
+
+using autotrader::get_log_stream;
+
+int main(int argc, char** argv)
+{
+  if (argc != 3)
+  {
+    throw std::runtime_error("Expecting host and port arguments");
+  }
+  std::string host{argv[1]}; // host.docker.internal
+  int port{std::atoi(argv[2])}; // 7497
+  autotrader::IBClient client{host, port};
+
+  Contract contract;
+  contract.symbol = "TSLA";
+  contract.secType = "STK";
+  contract.currency = "USD";
+  contract.exchange = "SMART";
+  client.start();
+
+  size_t index = client.start_market_data_stream(contract);
+
+  //std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+  client.stop_market_data_stream(index);
+}
+

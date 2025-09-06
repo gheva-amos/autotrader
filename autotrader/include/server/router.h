c168828572e5bc43b12d0666b21a75f34c3a8974
Amos Gheva <ghevaamos@gmail.com>
2025-09-05 20:30:39 -0400

    Extending the router functionality to handle data requests

--- autotrader/include/server/router.h
+++ autotrader/include/server/router.h
@@ -9,9 +9,11 @@ namespace autotrader
 class Router : public BaseThread
 {
 public:
-  Router(zmq::context_t& ctx, std::string address, zmq::socket_type type);
+  Router(zmq::context_t& ctx, std::string address, IBClient& ib);
   virtual void step() override;
 private:
+  void process_data_req(std::vector<zmq::message_t>& frames);
+  void stop_data_req(std::vector<zmq::message_t>& frames);
 };
 
 } // namespace
--- autotrader/src/server/router.cpp
+++ autotrader/src/server/router.cpp
@@ -1,10 +1,11 @@
 #include "server/router.h"
+#define DEBUG
 #include "debug.h"
 namespace autotrader
 {
 
-Router::Router(zmq::context_t& ctx, std::string address, zmq::socket_type type) :
-  BaseThread(ctx, address, type)
+Router::Router(zmq::context_t& ctx, std::string address, IBClient& ib) :
+  BaseThread(ctx, address, ib, zmq::socket_type::router)
 {
 }
 
@@ -12,10 +13,6 @@ void Router::step()
 {
   std::vector<zmq::message_t> frames;
   if (!recv_all(frames))
-  {
-    return;
-  }
-  if (frames.size() != 3)
   {
     return;
   }
@@ -28,6 +25,32 @@ void Router::step()
     send(frames[1], true);
     send("OK");
   }
+  else if (verb == "data")
+  {
+    process_data_req(frames);
+  }
+  else if (verb == "stop_data")
+  {
+    stop_data_req(frames);
+  }
+}
+
+void Router::process_data_req(std::vector<zmq::message_t>& frames)
+{
+  auto symbol = frames[3].to_string();
+  size_t req_id = start_data_for_symbol(symbol);
+  send(frames[0], true);
+  send(req_id);
+}
+
+void Router::stop_data_req(std::vector<zmq::message_t>& frames)
+{
+  size_t req_id = std::atol(frames[3].to_string().c_str());
+  DBG_MSG(req_id) <<frames[0].to_string()<<std::endl;
+  stop_data_for_id(req_id);
+  send(frames[0], true);
+  send(frames[3], true);
+  send("OK");
 }
 
 } // namespace

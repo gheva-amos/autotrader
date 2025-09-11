3fc9fff6fe2040de2fa7f58d0e3690c3da1ec165
Amos Gheva <ghevaamos@gmail.com>
2025-09-11 11:56:42 -0400

    parsing the xml from ib

--- python/autotrader/tools/scanner_params.py
+++ python/autotrader/tools/scanner_params.py
@@ -11,20 +11,100 @@ class ScannerParams:
 
   @staticmethod
   def text(el, name, default=""):
-      c = el.find(name)
+      c = el.get(name)
+      if not c:
+        c = el.find(name)
       return c.text.strip() if (c is not None and c.text) else default
 
-  def parse(self):
+  @staticmethod
+  def filter_list(string):
+    return string.split(',')
+
+  def walk_location_tree(self, location_root):
+    locations = location_root.findall('./Location')
+    for location in locations:
+      name = self.text(location, 'displayName')
+      code = self.text(location, 'locationCode')
+      instrument = self.text(location, 'instruments')
+      route_exchange = self.text(location, 'routeExchange')
+      self.locations.append({'name': name, 'code': code, 'instrument': instrument, 'route_exchange': route_exchange})
+      children = location.find('./LocationTree')
+      if children is not None:
+        self.walk_location_tree(children)
+
+  def parse_instruments(self):
     for il in self.root.findall("./InstrumentList"):
-      print(1)
+      if il.attrib['varName'] == 'fullInstrumentList':
+        continue
       for inst in il.findall("./Instrument"):
         self.instruments.append({
-          "type": inst.get("type") or self.text(inst, "type"),
-          "secType": inst.get("secType") or self.text(inst, "secType"),
-          "filters": inst.get("filters") or self.text(inst, "filters"),
-          "name": inst.get("name") or self.text(inst, "name"),
-          "group": inst.get("group") or self.text(inst, "group"),
-          "assetClass": self.text(inst, "assetClass"),
+          "type": self.text(inst, "type"),
+          "secType": self.text(inst, "secType"),
+          "filters": self.filter_list(self.text(inst, "filters")),
+          "name": self.text(inst, "name"),
+          "group": self.text(inst, "group"),
           })
-    print(self.instruments)
-    pass
+
+  def parse_combo_fields(self, field):
+    ret = []
+    values = field.findall('./ComboValue')
+    for value in values:
+      ret.append({
+        'code': self.text(value, 'code')
+      })
+    return ret
+
+  def parse_field(self, field):
+    ret = {}
+    ret['code'] = self.text(field, 'code')
+    ret['name'] = self.text(field, 'displayName')
+    combo_fields = field.find('./ComboValues')
+    if combo_fields is None:
+      ret['min'] = self.text(field, 'minValue')
+      ret['max'] = self.text(field, 'maxValue')
+    else:
+      ret['combo'] = self.parse_combo_fields(combo_fields)
+    return ret
+
+  def parse_simple_filter(self, filt):
+    elem = {}
+    elem['id'] = self.text(filt, 'id')
+    elem['category'] = self.text(filt, 'category')
+    elem['access'] = self.text(filt, 'access')
+    field = filt.find('./AbstractField')
+    elem['fields'] = (self.parse_field(field))
+    self.filters.append(elem)
+
+  def parse_range_filter(self, filt):
+    elem = {}
+    elem['id'] = self.text(filt, 'id')
+    fields = filt.findall('./AbstractField')
+    elem['fields'] = (self.parse_field(fields[0]), self.parse_field(fields[1]))
+    self.filters.append(elem)
+
+  def parse_filters(self):
+    filter_list = self.root.find('./FilterList')
+    simple_filters = filter_list.findall('./SimpleFilter')
+
+    for sf in simple_filters:
+      self.parse_simple_filter(sf)
+
+    range_filters = filter_list.findall('./RangeFilter')
+    for rf in range_filters:
+      self.parse_range_filter(rf)
+
+  def parse_scan_types(self):
+    for st in self.root.findall('./ScanTypeList/ScanType'):
+      elem = {}
+      elem['name'] = self.text(st, 'displayName')
+      elem['code'] = self.text(st, 'scanCode')
+      elem['instruments'] = self.text(st, 'instruments').split(',')
+      self.scan_types.append(elem)
+
+  def parse(self):
+    self.parse_instruments()
+    location_root = self.root.find("./LocationTree")
+    self.walk_location_tree(location_root)
+    self.parse_filters()
+    self.parse_scan_types()
+

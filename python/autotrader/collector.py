c981fc3750fdbd67b8d6913e2c9ceeee14c1148b
Amos Gheva <ghevaamos@gmail.com>
2025-09-26 17:08:39 -0400

    Collecting data frames from the bar collector model

--- python/autotrader/collector.py
+++ python/autotrader/collector.py
@@ -2,15 +2,35 @@ from autotrader.lib.working_thread import WorkingThread
 
 import zmq
 import queue
+import json
+import pandas as pd
+import io
+import os
+import tempfile
 
 class Collector(WorkingThread):
   def __init__(self, host):
     super().__init__("collector", host, host, zmq.PULL, None, True)
+
+  def atomic_write_file(self, path, data):
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+    with tempfile.NamedTemporaryFile(dir=os.path.dirname(path), delete=False) as tmp:
+      tmp.write(data)
+      tmp.flush()
+      os.fsync(tmp.fileno())
+      tmp_path = tmp.name
+    os.replace(tmp_path, path)
 
   def step(self):
     try:
       frames = self.inbox.get_nowait()
     except queue.Empty:
       return
-    print(frames)
+
+    if frames[1].decode() == 'indicators_pd':
+      symbol = frames[2].decode()
+      print(symbol)
+      path = os.path.join('./data', symbol + '.parquet')
+      data = io.BytesIO(frames[3])
+      self.atomic_write_file(path, data.getvalue())
 

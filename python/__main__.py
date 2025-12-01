178a8892c921b936bafe1d0ee4ce2cc39d551f4a
Amos Gheva <ghevaamos@gmail.com>
2025-12-01 10:21:44 -0500

    exposing the main as a function

--- python/__main__.py
+++ python/__main__.py
@@ -2,6 +2,9 @@ import sys
 from pathlib import Path
 
 if __name__ == "__main__":
+  main()
+
+def main():
   PKG_ROOT = Path(__file__).resolve().parent
   if str(PKG_ROOT) not in sys.path:
     sys.path.insert(0, str(PKG_ROOT))
